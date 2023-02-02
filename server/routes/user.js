a5ee17bccb55408c0d149fad9f7f6ded071bd84a
t4h3r <taherjalaljuma@gmail.com>
2023-02-02 04:49:41 +0400

    edit??

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,5 +7,5 @@ const isLoggedIn = require('../helper/isLoggedIn')
 const userCntrl = require('../controller/user')
 
 router.get('/user' , userCntrl.userDetails_get)
-router.put('/user' , isLoggedIn , userCntrl.user_edit_put)
+router.put('/user' , userCntrl.user_edit_put)
 module.exports = router
